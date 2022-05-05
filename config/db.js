e1f13b0b12ae06ba0c5fdaa64da9cb622c2ce553
Olamide <78960651+OlamideSimon@users.noreply.github.com>
2022-05-05 09:24:06 +0100

    crime-records/users api

--- config/db.js
+++ config/db.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose')
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(
+            process.env.MONGO_URI,
+            {
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+              }
+        )
 
         console.log(`MongoDb Connected: ${conn.connection.host}`);
     } catch (error) {
